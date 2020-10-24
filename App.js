fd2f5b7b0183c90bed564f92203c025a1cdac6a3
jowkha <jowkha460@gmail.com>
2020-10-24 23:07:08 +0700

    up

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 2,
+    flex: 1,
     backgroundColor: "blue",
     alignItems: "center",
     justifyContent: "center",
