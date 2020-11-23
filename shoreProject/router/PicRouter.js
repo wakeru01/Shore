cf0c0ef32c759be05fcaa6872105736161469bc9
wakeru01 <42973275+wakeru01@users.noreply.github.com>
2020-11-24 01:25:51 +0700

    header

--- shoreProject/router/PicRouter.js
+++ shoreProject/router/PicRouter.js
@@ -7,9 +7,9 @@ const Stack = createStackNavigator()
 
 export default function PicRouter() {
     return (
-        <Stack.Navigator initialRouteName="AddSheet" headerShown={false}>
-          <Stack.Screen name="AddPic" component={AddPicScreen} />
-          <Stack.Screen name="AddSheet" component={AddSheetScreen} />
+        <Stack.Navigator initialRouteName="AddSheet" headerShown={false} >
+          <Stack.Screen name="AddPic" component={AddPicScreen} options={{headerStyle: {backgroundColor: '#3198cc'},headerTintColor: "white",}}/>
+          <Stack.Screen name="AddSheet" component={AddSheetScreen} options={{headerStyle: {backgroundColor: '#3198cc'},headerTintColor: "white",}}/>
         </Stack.Navigator>
       )
 }
