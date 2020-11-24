325d74261681bf16d25dfa8af5bc1a4955b461ff
Rungwarapon <42974200+Rungwarapon@users.noreply.github.com>
2020-11-24 23:22:54 +0700

    profile

--- shoreProject/screens/main/ProfileScreen.js
+++ shoreProject/screens/main/ProfileScreen.js
@@ -3,6 +3,9 @@ import { View, StyleSheet, Text, Image, Dimensions, StatusBar, TouchableOpacity 
 import { TabView, SceneMap } from 'react-native-tab-view';
 import firebase from 'firebase/app'
 import 'firebase/auth'
+import 'firebase/firestore'
+import { FlatList } from 'react-native-gesture-handler';
+
 // import PhotoUpload from 'react-native-photo-upload';
 
 // const Profile = () => {
@@ -23,27 +26,105 @@ import 'firebase/auth'
         
 //   )
 // }
+  // var db = firebase.firestore()
+    
+  //   const [keepSheet, setSheet] = React.useState([]);
+  // useEffect(() => {
+  //   const didMount = async () => {
+  //       let keepSheet = []
+  //       const db = firebase.firestore()
+  //       const docs = await db.collectionGroup('sheets').get()
+  //       docs.forEach(s => {
+  //           keepSheet = [...keepSheet, {...s.data(), id: s.id}]
+  //       })
+  //       console.log(keepSheet)
+  //       setSheet(keepSheet)
+  //       setFilterSheet(keepSheet)
+  //   }
+  //   didMount()
+  // }, []);
+  // const navDetail = (payload) => {
+  //   props.navigation.push('Detail', {
+  //       id: payload
+  //   })
+  // }
+//   const renderItem = ({ item }) => (
+//     <View style={styles.gridTile}>
+//         <View style={styles.imageGride}>
+//             <Image style={styles.pic} source={require("../../assets/file.png")} />
+//         </View>
+//         <View style={styles.detailGride}>
+//             <Text style={styles.header}>{item.subject}</Text>
+//             <View style={styles.ratingStr}>
+//                 <Image style={styles.picProfile} source={require("../../assets/dot.png")} />
+//                 <Text> ปีการศึกษา : {item.year}  ภาคเรียนที่ : {item.semester}</Text>
+//             </View>
+//             <View style={styles.ratingStr}>
+//                 <Text>Rating : </Text>
+//                 <Image style={styles.picStar} source={require("../../assets/star.png")} />
+//                 <Image style={styles.picStar} source={require("../../assets/star.png")} />
+//                 <Image style={styles.picStar} source={require("../../assets/star.png")} />
+//             </View>
+//         </View>
+//         <View style={styles.detailPrice}>
+//             <Text style={styles.price}>{item.price}฿</Text>
+//             <TouchableOpacity onPress={() => {
+//                 navDetail(item.id)
+//             }}>
+//             <Image style={styles.picNext} source={require("../../assets/next.png")} />
+//             </TouchableOpacity>
+
+//         </View>
+//     </View>
+// );
 
 const FirstRoute = () => (
   <View>
     <View style={[styles.scene, { backgroundColor: 'white' }]} />
-      <View style={styles.gridTile}>
-        <View style={styles.imageGride}>
-            <Image style={styles.pic} source={require("../../assets/file.png")} />
+    <View style={styles.gridTile}>
+            <View style={styles.imageGride}>
+                <Image style={styles.pic} source={require("../../assets/file.png")} />
+            </View>
+            <View style={styles.detailGride}>
+                <Text style={styles.header}>วิชา : Mobile Device</Text>
+                <Text>คณะ : เทคโนโลยีสารสนเทศ</Text>
+                <Text>ภาควิชา : เทคโนโลยีสารสนเทศ</Text>
+                <Text>ปีการศึกษา : 2018</Text>
+                <Text>ภาคเรียนที่ : 2</Text>
+                <Text>ราคา : 50 บาท</Text>
+            </View>
+            <View style={styles.detailPrice}>
+            <TouchableOpacity onPress={() => {
+                }}>
+                <Image style={styles.picNext} source={require("../../assets/x.png")} />
+            </TouchableOpacity>
+            </View>
         </View>
-      </View>
   </View>
 );
 
 const SecondRoute = () => (
   <View>
     <View style={[styles.scene, { backgroundColor: 'white' }]} />
-    <View style={styles.grid}>
-      <View style={styles.gridTile}></View>
-      <View style={styles.gridTile}></View>
-      <View style={styles.gridTile}></View>
-      <View style={styles.gridTile}></View>
-    </View>
+    <View style={styles.gridTile}>
+            <View style={styles.imageGride}>
+                <Image style={styles.pic} source={require("../../assets/file.png")} />
+            </View>
+            <View style={styles.detailGride}>
+                <Text style={styles.header}>วิชา : Human Design</Text>
+                <Text>คณะ : เทคโนโลยีสารสนเทศ</Text>
+                <Text>ภาควิชา : เทคโนโลยีสารสนเทศ</Text>
+                <Text>ปีการศึกษา : 2018</Text>
+                <Text>ภาคเรียนที่ : 2</Text>
+                <Text>ราคา : 50 บาท</Text>
+            </View>
+            <View style={styles.detailPrice}>
+            <TouchableOpacity onPress={() => {
+                }}>
+                <Image style={styles.picNext} source={require("../../assets/x.png")} />
+            </TouchableOpacity>
+            </View>
+        </View>
   </View>
 );
 
@@ -53,8 +134,8 @@ export default function Profile(props) {
   const user = firebase.auth().currentUser;
   const [index, setIndex] = React.useState(0);
   const [routes] = React.useState([
-    { key: 'first', title: 'Buy' },
-    { key: 'second', title: 'Sell' },
+    { key: 'first', title: 'ซื้อ' },
+    { key: 'second', title: 'ขาย' },
   ]);
 
   const renderScene = SceneMap({
@@ -119,20 +200,26 @@ const styles = StyleSheet.create({
     paddingLeft: 2,
     marginTop: 7,
     borderRadius: 3,
-    flexDirection: 'row',
-  },
-  gridTile:{
-    backgroundColor: "white",
-    padding:10,
-    paddingLeft:2,
-    marginTop:7,
-    borderRadius:3,
-    flexDirection:'row'
-  },
-  imageGride:{
-      flex: 1,
+    flexDirection: 'row'
   },
   header:{
       fontSize:18
   },
-});
+  imageGride: {
+    flex: 1,
+},
+  detailGride: {
+      flex: 2
+  },
+  picNext: {
+      width: 10,
+      height: 10,
+  },
+  detailPrice: {
+      flexDirection: 'column'
+  },
+  pic: {
+      width: 100,
+      height: 100
+  },
+  });
